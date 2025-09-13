fb80ff5b3901060206264d532ce86c793b5be3f1
quyntg <quyntg94@gmail.com>
2025-09-13 18:42:20 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -251,7 +251,6 @@ function showResultModal(counter) {
                 // Lệnh ESC/POS
                 let text = [
                     // Bỏ lệnh reset ESC @ để tránh bị in ký tự @
-                    '\x1D\x56\x41', // Cắt giấy
                     '\x1B\x61\x01', // Căn giữa
                     '\x1D\x21\x01', // Font nhỏ (width x2, height x1)
                     removeVietnameseTones('UY BAN NHAN DAN XA TAY DO') + '\n',
@@ -265,9 +264,10 @@ function showResultModal(counter) {
                     '\x1D\x21\x01', // Quay lại font nhỏ cho dòng dưới
                     '\n',
                     removeVietnameseTones('Vui long cho den luot') + '\n',
-                    // Thêm đoạn trắng dài để dễ cắt
-                    '\n\n\n\n\n\n\n\n\n\n',
-                    '\x1D\x21\x00' // Trở lại font thường
+                    // Thêm đoạn trắng vừa đủ để thuận tiện lấy số
+                    '\n\n\n\n\n',
+                    '\x1D\x21\x00', // Trở lại font thường
+                    '\x1D\x56\x41' // Cắt giấy
                 ];
                 const data = [
                     { type: 'raw', format: 'plain', data: text.join('') }
